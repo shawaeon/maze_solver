4901eb01883a238ee4672085bbeba5c47955fe48
shawaeon <jens.sjogr@gmail.com>
2024-12-27 18:09:54 +0200

    add solve method to Maze

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ def main():
 
     num_rows = 10
     num_cols = 14
-    margin = 150
-    screen_x = 1200
-    screen_y = 1000
+    margin = 50
+    screen_x = 800
+    screen_y = 600
     cell_size_x = (screen_x - (2 * margin)) / num_cols
     cell_size_y = (screen_y - (2 * margin)) / num_rows
 
     win = Window(screen_x, screen_y)
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, window=win, seed=10)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, window=win)
+    print("Maze created.")
+
+    is_solvable = maze.solve()
+    if is_solvable:
+        print("Maze solved!")
+    else:
+        print("Maze is not solvable.")
 
     win.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -2,6 +2,7 @@ from cell import Cell
 import time
 import random
 
+
 class Maze:
     def __init__(
         self,
@@ -31,6 +32,63 @@ class Maze:
         self._break_walls_r(0, 0)
         self._reset_cells_visited()
 
+
+    # tries to solve the maze
+    # returns True if maze is solved
+    # returns False if maze is unsolvable
+    def solve(self):
+        return self._solve_r(0, 0)
+    
+    
+    # returns True if end of the maze
+    # returns False if dead-end
+    def _solve_r(self, i, j):
+        self._animate()
+        
+        # marks current cell visited
+        self._cells[i][j].visited = True
+        
+        # returns True if at the end cell
+        if i == self._num_cols - 1 and j == self._num_rows - 1:
+            return True
+        
+        # visits left cell if way is open and hasn't been visited        
+        if 0 <= i - 1 and not self._cells[i][j].has_left_wall and not self._cells[i - 1][j].visited:
+            self._cells[i][j].draw_move(self._cells[i - 1][j])
+            if self._solve_r(i - 1, j):
+                return True
+            # if path end in dead-end marks with grey line
+            else:
+                self._cells[i][j].draw_move(self._cells[i - 1][j], undo=True)
+        
+        # visits right cell
+        if i + 1 < len(self._cells) and not self._cells[i][j].has_right_wall and not self._cells[i + 1][j].visited:
+            self._cells[i][j].draw_move(self._cells[i + 1][j])
+            if self._solve_r(i + 1, j):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i + 1][j], undo=True)
+
+        # visits top cell
+        if 0 <= j - 1 and not self._cells[i][j].has_top_wall and not self._cells[i][j - 1].visited:
+            self._cells[i][j].draw_move(self._cells[i][j - 1])
+            if self._solve_r(i, j - 1):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i][j - 1], undo=True)
+
+        # visits bottom cell
+        if j + 1 < len(self._cells[0]) and not self._cells[i][j].has_bottom_wall and not self._cells[i][j + 1].visited:
+            self._cells[i][j].draw_move(self._cells[i][j + 1])
+            if self._solve_r(i, j + 1):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i][j + 1], undo=True)
+
+        # lets previous cell know this is a dead-end
+        return False
+        
+
     def _create_cells(self):        
         for i in range(self._num_cols):
             self._cells.append([])
@@ -38,11 +96,13 @@ class Maze:
                 self._cells[i].append(Cell(self._win))
                 self._draw_cell(i, j)
 
+
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
         self._draw_cell(0, 0)
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+
 
     def _break_walls_r(self, i, j):
         self._cells[i][j].visited = True
@@ -83,10 +143,12 @@ class Maze:
 
             self._break_walls_r(next_index[0], next_index[1])
 
+
     def _reset_cells_visited(self):
         for column in self._cells:
             for cell in column:
                 cell.visited = False
+
 
     def _draw_cell(self, i, j):
         cell_x1 = self._x1 + i * self._cell_size_x        
@@ -98,8 +160,7 @@ class Maze:
 
         self._animate()
 
+
     def _animate(self):
         self._win.redraw()
-        time.sleep(0.01)
-        
-
+        time.sleep(0.05)
